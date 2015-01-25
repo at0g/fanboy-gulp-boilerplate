c0f43f59e7d584b1bcd08c4b894f3e0fec7e500b
Ben Daley <benjamindaley@gmail.com>
2015-01-25 12:49:06 +1100

    Add support for images. hash revisions for images, replacements in styl -> css pipe (images hashed, then css created), source maps maintained. Hash revisions in handlebars templates.

--- gulpfile.js
+++ gulpfile.js
@@ -7,58 +7,145 @@ var gulp        = require('gulp'),
     please      = require('gulp-pleeease'),
     handlebars  = require('gulp-compile-handlebars'),
     rename      = require('gulp-rename'),
-    del         = require('del')
+    replace     = require('gulp-batch-replace'),
+    imagemin    = require('gulp-imagemin'),
+    del         = require('del'),
+    runSequence = require('run-sequence')
 ;
 
 
-gulp.task('clean:css', function(cb){
-    del(['./dist/css'], cb);
+
+var paths = {
+    src: {
+        dir: path.join(process.cwd(), 'src'),
+        images: {
+            dir: path.join(process.cwd(), 'src/assets/img')
+        },
+        styles: {
+            dir: path.join(process.cwd(), 'src/styles'),
+            includes: [
+                path.join(process.cwd(), 'src/stylus-lib')
+            ]
+        },
+        templates: {
+            dir: 'src/templates/'
+        }
+    },
+    build: {
+        dir: path.join(process.cwd(), 'dist'),
+        css: {
+            dir: './dist/css'
+        },
+        images: {
+            dir: './dist/assets/images'
+        }
+    }
+};
+
+
+
+gulp.task('clean:build', function(cb){
+    del([paths.build.dir], cb);
 });
 
+gulp.task('clean:css', function(cb){
+    del([paths.build.css.dir], cb);
+});
 
-gulp.task('stylus', ['clean:css'], function(){
-    return gulp.src('./src/css/**/*.styl', { base: './src/css' })
+gulp.task('clean:images', function(cb){
+    del([paths.build.images.dir], cb);
+});
+
+gulp.task('stylus', function(){
+    var manifest = JSON.parse(fs.readFileSync(paths.build.dir + '/bundle.json', 'utf8')),
+        replacements = [],
+        keys = Object.keys(manifest),
+        isImg = /\.(png|jpg|gif|svg)/
+        ;
+
+    for(var i = 0; i<keys.length; i++){
+        var key = keys[i];
+        if(key.match(isImg)){
+            replacements.push([ key, '/' + manifest[key] ]);
+        }
+    }
+
+    return gulp.src(paths.src.styles.dir + '/**/*.styl')
         .pipe( sourcemaps.init() )
+        .pipe( replace( replacements ))
         .pipe( stylus({
-            paths: [
-                path.join(__dirname, './src/css'),
-                path.join(__dirname, './src/stylus-lib')
-            ]
+            paths: paths.src.styles.includes
         }) )
         .pipe( please({ minifier: false }) )
         .pipe( sourcemaps.write('.') )
-        .pipe( gulp.dest('./dist/css') )
+        .pipe( gulp.dest(paths.build.css.dir ) )
 });
 
-
-gulp.task('bundle', ['stylus'], function(){
-
-    return gulp.src('./dist/css/*.css', { base: path.join(process.cwd(), 'dist') })
-        .pipe( sourcemaps.init({ loadMaps: true }) )
+gulp.task('imagemin', function(){
+    return gulp.src(paths.src.images.dir + '/**/*', { base: paths.src.dir } )
+        .pipe( imagemin() )
         .pipe( rev() )
-        .pipe( sourcemaps.write('.') )
-        .pipe( gulp.dest('./dist') )
+        .pipe( gulp.dest(paths.build.dir) )
         .pipe( rev.manifest('bundle.json', {
-            base: path.join(process.cwd(), 'dist'),
-            merge :true
+            base: paths.build.dir, merge: true
         }) )
-        .pipe( gulp.dest('dist/bundle.json') )
+        .pipe( gulp.dest(paths.build.dir + '/bundle.json') )
     ;
 });
 
-gulp.task('compile:html', ['bundle'], function(){
-    var manifest = JSON.parse(fs.readFileSync(__dirname + '/dist/bundle.json', 'utf8')),
+gulp.task('handlebars', function(cb){
+    var manifest = JSON.parse(fs.readFileSync(paths.build.dir + '/bundle.json', 'utf8')),
         helpers  = require('./src/templates/helpers')
-    ;
+        ;
     return gulp.src('./src/templates/index.hbs')
         .pipe( handlebars(manifest, { helpers: helpers }) )
         .pipe( rename('index.html') )
-        .pipe( gulp.dest('./dist') )
+        .pipe( gulp.dest(paths.build.dir) )
+        ;
+});
+
+
+gulp.task('bundle:css', function(){
+
+    return gulp.src( paths.build.css.dir + '/**/*.css', { base: paths.build.dir })
+        .pipe( sourcemaps.init({ loadMaps: true }) )
+        .pipe( rev() )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest(paths.build.dir) )
+        .pipe( rev.manifest({
+            base: paths.build.dir,
+            merge: true,
+            path: paths.build.dir + '/bundle.json'
+        }) )
+        .pipe( gulp.dest(paths.build.dir) )
     ;
 });
 
-gulp.task('watch', function(){
-    gulp.watch(['./src/css/**/*.styl', './src/stylus-lib/**/*.styl'], ['compile:html']);
+gulp.task('compile:with-images', function(cb){
+    runSequence(
+        'clean:images',
+        'imagemin',
+        'compile'
+    )
 });
 
-gulp.task('default', ['watch', 'compile:html']);
+gulp.task('compile', function(cb){
+    runSequence(
+        'clean:css',
+        'stylus',
+        'bundle:css',
+        'handlebars'
+    )
+});
+
+gulp.task('watch', function(){
+
+    var stylusFilesToWatch = [paths.src.styles.dir + '/**/*.styl'].concat(paths.src.styles.includes.map(function(p){
+        return p + '/**/*.styl'
+    }));
+    gulp.watch(stylusFilesToWatch, ['compile']);
+    gulp.watch(paths.src.images.dir + '**/*', ['compile:with-images']);
+    gulp.watch(paths.src.templates.dir + '**/*', ['handlebars'] );
+});
+
+gulp.task('default', ['watch', 'compile:with-images']);
